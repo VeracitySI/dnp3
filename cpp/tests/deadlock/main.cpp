849d13ffc64b082ebdb8fad2515c558aa43d298f
jadamcrain <jadamcrain@gmail.com>
2018-07-05 12:58:43 -0700

    number of thread-pool threads is not a factor in deadlock

--- cpp/tests/deadlock/main.cpp
+++ cpp/tests/deadlock/main.cpp
@@ -15,29 +15,39 @@ using namespace asiopal;
 using namespace asiodnp3;
 using namespace opendnp3;
 
+const uint32_t FILTERS = levels::NOTHING;
+const uint32_t NUM_THREADS = 1;
+
+void start_outstation(DNP3Manager& manager)
+{
+	const auto channel = manager.AddTCPServer("server", FILTERS, ServerAcceptMode::CloseNew, "127.0.0.1", 20000, PrintingChannelListener::Create());
+	const auto outstation = channel->AddOutstation("outstation", SuccessCommandHandler::Create(), DefaultOutstationApplication::Create(), OutstationStackConfig(DatabaseSizes::AllTypes(0)));
+	outstation->Enable();
+}
+
+void start_master(DNP3Manager& manager)
+{
+	const auto channel = manager.AddTCPClient("client", FILTERS, ChannelRetry(TimeDuration::Seconds(0), TimeDuration::Seconds(0)), "127.0.0.1", "127.0.0.1", 20000, PrintingChannelListener::Create());
+	const auto master = channel->AddMaster("master", PrintingSOEHandler::Create(), asiodnp3::DefaultMasterApplication::Create(), MasterStackConfig());
+	const auto scan = master->AddClassScan(ClassField::AllClasses(), TimeDuration::Milliseconds(1));
+	master->Enable();
+}
+
 int main()
 {
-	const uint32_t FILTERS = levels::NOTHING;
 
 	for(int i = 0; i < 100000; ++i)
 	{
-		// create an outstation
-		auto manager2 = std::make_unique<DNP3Manager>(4, ConsoleLogger::Create());
-		auto channel2 = manager2->AddTCPServer("server", FILTERS, ServerAcceptMode::CloseNew, "127.0.0.1", 20000, PrintingChannelListener::Create());
-		auto outstation = channel2->AddOutstation("outstation", SuccessCommandHandler::Create(), DefaultOutstationApplication::Create(), OutstationStackConfig(DatabaseSizes::AllTypes(0)));
-		outstation->Enable();
+		DNP3Manager manager1(NUM_THREADS, ConsoleLogger::Create());
+		DNP3Manager manager2(NUM_THREADS, ConsoleLogger::Create());
 
-		// create a master
-		auto manager = std::make_unique<DNP3Manager>(4, ConsoleLogger::Create());
-		auto channel = manager->AddTCPClient("client", FILTERS, ChannelRetry(TimeDuration::Seconds(0), TimeDuration::Seconds(0)), "127.0.0.1", "127.0.0.1", 20000, PrintingChannelListener::Create());
-		auto master = channel->AddMaster("master", PrintingSOEHandler::Create(), asiodnp3::DefaultMasterApplication::Create(), MasterStackConfig());
-		auto integrityScan = master->AddClassScan(ClassField::AllClasses(), TimeDuration::Milliseconds(1));
-		master->Enable();
+		start_outstation(manager1);
+		start_master(manager2);
 
 		// give the two sides sufficient time to connect
 		std::this_thread::sleep_for(std::chrono::milliseconds(5));
 
-		// let things shutdown in the order they are allocated
+		// shutdown manager2 followed by manager1
 	}
 
 	return 0;
